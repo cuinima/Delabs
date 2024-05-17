9cf06b4c9eef5e45637bdfd5b71510a380b01f69
cuinima <65176389+cuinima@users.noreply.github.com>
2024-05-17 14:07:47 +0800

    Update start.py

--- src/start.py
+++ src/start.py
@@ -10,39 +10,53 @@ import pandas as pd
 async def DailyDelab(thread: int):
     logger.info(f"Thread {thread} | Start work")
     while True:
-        act = await random_line('data/accounts.txt')
-        if not act: break
+        try:
+            act = await random_line('data/accounts.txt')
+            if not act: break
 
-        if '::' in act:
-            private_key, proxy = act.split('::')
-        else:
-            private_key = act
-            proxy = None
-
-        delabs = Delabs(key=private_key, thread=thread, proxy=proxy)
-
-        if await delabs.login():
-            if await delabs.set_referrer():
-                logger.success(f"Thread {thread} | Set referral code {config.REF_CODE}! Address: {delabs.web3_utils.acct.address}")
-
-            if await delabs.check_in():
-                logger.success(f"Thread {thread} | Completed daily check in! Address: {delabs.web3_utils.acct.address}")
+            if '::' in act:
+                private_key, proxy = act.split('::')
             else:
-                logger.warning(f"Thread {thread} | Already completed daily check in! Address: {delabs.web3_utils.acct.address}")
+                private_key = act
+                proxy = None
 
-            if await delabs.draw():
-                logger.success(f"Thread {thread} | Completed daily draw! Address: {delabs.web3_utils.acct.address}")
-            else:
-                logger.warning(f"Thread {thread} | Already completed daily draw! Address: {delabs.web3_utils.acct.address}")
+            delabs = Delabs(key=private_key, thread=thread, proxy=proxy)
 
-        await delabs.sleep(random.uniform(config.DELAY[0], config.DELAY[1]))
-        await delabs.logout()
+            if await delabs.login():
+                code = random.choice(config.REF_CODE)
+                if await delabs.set_referrer(code):
+                    logger.success(f"Thread {thread} | Set referral code {config.REF_CODE}! Address: {delabs.web3_utils.acct.address}")
+
+                if await delabs.check_in():
+                    logger.success(f"Thread {thread} | Completed daily check in! Address: {delabs.web3_utils.acct.address}")
+                else:
+                    logger.warning(f"Thread {thread} | Already completed daily check in! Address: {delabs.web3_utils.acct.address}")
+
+                if await delabs.draw():
+                    logger.success(f"Thread {thread} | Completed daily draw! Address: {delabs.web3_utils.acct.address}")
+                else:
+                    logger.warning(f"Thread {thread} | Already completed daily draw! Address: {delabs.web3_utils.acct.address}")
+
+                if await delabs.getPoints():
+                    logger.success(f"Thread {thread} | Completed getPoints! Address: {delabs.web3_utils.acct.address}")
+                else:
+                    logger.warning(
+                        f"Thread {thread} | Already completed getPoints! Address: {delabs.web3_utils.acct.address}")
+                res = await delabs.get_user_info()
+                logger.success(f"Thread {thread} | Completed get_user_info! Address: {delabs.web3_utils.acct.address}  Points:{res[2]}")
+
+
+            await delabs.sleep(random.uniform(config.DELAY[0], config.DELAY[1]))
+            await delabs.logout()
+        except BaseException as e:
+            logger.error(e)
 
     logger.info(f"Thread {thread} | Finish work")
 
 
 async def StatsDelay():
-    accounts = get_all_lines('data/accounts.txt')
+    reffers = []
+    accounts = get_all_lines('data/accounts.txt')[:20]
     if not accounts:
         logger.warning(f"No accounts in data/accounts.txt")
         return
@@ -59,10 +73,12 @@ async def StatsDelay():
 
         if await delabs.login():
             user_stats = await delabs.get_user_info()
+            reffers.append(user_stats[3])
             data.append(user_stats)
         await delabs.logout()
         logger.info(f"{num+1}/{len(accounts)} | Compiled stats for {delabs.web3_utils.acct.address}")
-
+    print(reffers)
+    return
     time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     path = f"data/statistics_{time}.csv"
 
